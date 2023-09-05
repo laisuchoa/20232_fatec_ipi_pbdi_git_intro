7c2702c36d4e0fdf2f15f0eddbea701a080f055d
lais.uchoa <lais.uchoa@outlook.com.br>
2023-09-04 22:59:51 -0300

    apostila02(git-python): funcao menu implementada

--- calculadora.py
+++ calculadora.py
@@ -9,3 +9,22 @@ def multiplicar(a, b):
 
 def dividir(a, b):
     return a / b
+
+def menu():
+    a = int(input("primeiro valor: "))
+    opcao = input("+ : somar\n- : subtrair\n* :  multiplicar\n/ : dividir\n")
+    b = int(input("segundo valor: "))
+    
+    if opcao == '+':
+        print(somar(a,b))
+    elif opcao == '-':
+        print(subtrair(a,b))
+    elif opcao == '*':
+        print(multiplicar(a,b))
+    elif opcao == '/':
+        print(dividir(a,b))
+    else:
+        print('operador inválido, tente novamente')
+        menu()
+        
+menu()
